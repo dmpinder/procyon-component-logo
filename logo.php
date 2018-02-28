89d9b1ef6b0c0879a4f3119e2c9110a3b4bcc60e
Sam Claughan <sam@sc89.me>
2018-02-28 11:28:51 +0000

    Meets WPCS, added gitignore for PHPStorm project folder, removed custom logo check when the custom logo wasn't actually used but an ACF field was.

--- logo.php
+++ logo.php
@@ -1,26 +1,33 @@
 <?php
 /*
-  Plugin Name: Procyon Component: Logo
-  Plugin URI: https://github.com/dmpinder/procyon-component-logo
-  Description: A drop-in PHP component to create a WCAG-compliant logo. Based on the excellent 10up component library.
-  Author: Darren Pinder, 10up, mattradford
-  Version: 1.0.1
-  Author URI: https://vatu.co.uk
-  GitHub Plugin URI: https://github.com/dmpinder/procyon-component-logo
-  GitHub Branch:     master
+ * Plugin Name: Procyon Component: Logo
+ * Plugin URI: https://github.com/dmpinder/procyon-component-logo
+ * Description: A drop-in PHP component to create a WCAG-compliant logo. Based on the excellent 10up component library.
+ * Author: Darren Pinder, 10up, mattradford
+ * Version: 1.0.2
+ * Author URI: https://vatu.co.uk
+ * GitHub Plugin URI: https://github.com/dmpinder/procyon-component-logo
+ * GitHub Branch:     master
  */
 
-function procyon_component_logo() { ?>
+/**
+ * Function to output a logo which is defined as an option.
+ */
+function procyon_component_logo() {
+	$logo = get_field( 'main_logo', 'options' );
+	if ( ! empty( $logo ) ) {
+		?>
+		<div itemscope itemtype="http://schema.org/Organization">
+			<!--
+				Visually hide this text.
+				WCAG 2.0: “image alt text cannot be the primary text of a link"
+			-->
+			<a href="<?php echo esc_url( home_url() ); ?>" class="custom-logo-link" rel="home" itemprop="url">
+				<img src="<?php echo ! empty( $logo ) ? esc_url( $logo ) : null; ?>" class="custom-logo" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" itemprop="logo">
+			</a>
 
-<?php if ( has_custom_logo() ) : ?>
-	<div itemscope itemtype="http://schema.org/Organization">
-		<?php the_custom_logo(); // The WordPress-provided function to create the logo ?>
-		<!--
-			Visually hide this text.
-			WCAG 2.0: “image alt text cannot be the primary text of a link"
-		-->
-		<span class="screen-reader-text"><?php echo get_bloginfo('name'); ?></span>
-	</div>
-<?php endif; ?>
-
-<?php } ?>
+			<span class="screen-reader-text"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></span>
+		</div>
+		<?php
+	}
+}
